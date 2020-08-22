1be677f9488d6c5338b0dc6f65438457580e379e
RicardoMattos780 <69880607+RicardoMattos780@users.noreply.github.com>
2020-08-21 20:31:19 -0400

    Add files via upload

---
+++ Calculadora/bootstrap/Index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+     <style>
+    <title>Prueba de PHP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </style>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ </body>
+</html>
